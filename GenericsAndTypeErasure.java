ff672088e845726a5de7f45bdb96aca4e03686d7
Hugonofre <hugoant7@gmail.com>
2026-09-15 00:27:43 -0600

    implement bounded generic types

--- GenericsAndTypeErasure.java
+++ GenericsAndTypeErasure.java
@@ -8,7 +8,22 @@ class Box<T> {
     T get() {
         return value;
     }
+}
 
+class NumericBox<T extends Number> {
+    private T value;
+
+    void set(T value) {
+        this.value = value;
+    }
+
+    T get() {
+        return value;
+    }
+
+    double doubleValue() {
+        return value.doubleValue();
+    }
 }
 
 public class GenericsAndTypeErasure {
@@ -21,5 +36,19 @@ public class GenericsAndTypeErasure {
 
         System.out.println(stringBox.getClass() == intBox.getClass());
         System.out.println(Box.class.getTypeParameters().length);
+
+        NumericBox<Integer> intNumBox = new NumericBox<>();
+        intNumBox.set(42);
+        System.out.println(intNumBox.getClass() == NumericBox.class);
+
+        NumericBox<Double> doubleNumBox = new NumericBox<>();
+        doubleNumBox.set(42.5);
+        System.out.println(intNumBox.doubleValue());
+        System.out.println(doubleNumBox.doubleValue());
+        // NumericBox<String> stringNumBox = new NumericBox<>(); // Error de
+        // compilación:
+        // String no extiende Number, y el bound "T extends Number" exige que cualquier
+        // tipo usado como T sea Number o una subclase suya. El compilador rechaza esto
+        // ANTES de generar bytecode, sin necesidad de ejecutar nada.
     }
 }
