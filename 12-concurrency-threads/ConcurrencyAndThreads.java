be61bc9cb11e3a1491aadf291e3ef722751eddde
Hugonofre <hugoant7@gmail.com>
2026-09-23 00:15:26 -0600

    demonstrate race condition with unsynchronized counter

--- 12-concurrency-threads/ConcurrencyAndThreads.java
+++ 12-concurrency-threads/ConcurrencyAndThreads.java
@@ -1,12 +1,28 @@
 public class ConcurrencyAndThreads {
     static int counter = 0;
-}
 
-static void incrementCounter(int times) {
-    for (int i = 0; i < times; i++) {
-        counter++;
+    static void incrementCounter(int times) {
+        for (int i = 0; i < times; i++) {
+            counter++;
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task = () -> incrementCounter(100_000);
+
+        Thread[] threads = new Thread[10];
+        for (int i = 0; i < threads.length; i++) {
+            threads[i] = new Thread(task);
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            thread.join();
+        }
+
+        System.out.println("Final counter: " + counter);
     }
 }
-
-public static void main(String[] args) {
-    Runnable task = () -> incrementCounter(100_000);
