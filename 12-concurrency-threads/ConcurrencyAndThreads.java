619105e89c93c391adbfbd483c4e735b050cbdf4
Hugonofre <hugoant7@gmail.com>
2026-09-23 23:48:20 -0600

    demonstrate race condition and synchronized exclusion with timing comparison

--- 12-concurrency-threads/ConcurrencyAndThreads.java
+++ 12-concurrency-threads/ConcurrencyAndThreads.java
@@ -1,5 +1,6 @@
 public class ConcurrencyAndThreads {
     static int counter = 0;
+    static int synchronizedCounter = 0;
 
     static void incrementCounter(int times) {
         for (int i = 0; i < times; i++) {
@@ -7,13 +8,28 @@ public class ConcurrencyAndThreads {
         }
     }
 
+    static synchronized void incrementSynchronized() {
+        synchronizedCounter++;
+    }
+
+    static void incrementSynchronizedCounter(int times) {
+        for (int i = 0; i < times; i++) {
+            incrementSynchronized();
+        }
+    }
+
     public static void main(String[] args) throws InterruptedException {
         Runnable task = () -> incrementCounter(100_000);
 
+        Runnable synchronizedTask = () -> incrementSynchronizedCounter(100_000);
+
         Thread[] threads = new Thread[10];
+
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(task);
         }
+
+        long unsynchronizedStart = System.nanoTime();
 
         for (Thread thread : threads) {
             thread.start();
@@ -23,6 +39,31 @@ public class ConcurrencyAndThreads {
             thread.join();
         }
 
+        long unsynchronizedEnd = System.nanoTime();
+        long unsynchronizedTime = unsynchronizedEnd - unsynchronizedStart;
+
+        Thread[] synchronizedThreads = new Thread[10];
+
+        for (int i = 0; i < synchronizedThreads.length; i++) {
+            synchronizedThreads[i] = new Thread(synchronizedTask);
+        }
+
+        long synchronizedStart = System.nanoTime();
+
+        for (Thread thread : synchronizedThreads) {
+            thread.start();
+        }
+
+        for (Thread thread : synchronizedThreads) {
+            thread.join();
+        }
+
+        long synchronizedEnd = System.nanoTime();
+        long synchronizedTime = synchronizedEnd - synchronizedStart;
+
         System.out.println("Final counter: " + counter);
+        System.out.println("Unsynchronized time: " + unsynchronizedTime + " ns");
+        System.out.println("Final synchronized counter: " + synchronizedCounter);
+        System.out.println("Synchronized time: " + synchronizedTime + " ns");
     }
 }
